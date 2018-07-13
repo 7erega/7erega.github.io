4a348a4f68cb25ae9fd66e8c1de4ad0a72ccb43d
7erega <melnyk376@gmail.com>
2018-07-13 17:27:41 +0300

    Add files via upload

---
+++ js/init/fullpage.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    sectionsColor: ['#fff', '#fff', '#f7f7f7', 'fff', '#fff'],
+    menu: '#mainMenu',
+    paddingTop: '105px',
+    fixedElements: '.main-header',
+    afterLoad: function(origin) {
+      console.log(origin);
+      if (origin === 'secondPage') {
+        $('.main-header').removeClass('bg-color-2');
+        $('.main-header').addClass('bg-color-1');
+      } else if (origin === 'thirdPage') {
+        $('.main-header').removeClass('bg-color-1');
+        $('.main-header').addClass('bg-color-2');
+      } else {
+        $('.main-header').removeClass('bg-color-1');
+        $('.main-header').removeClass('bg-color-2');
+      }
+    }
+  });
+});
