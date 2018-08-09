596e82b852b34fd5c4b59d1ad1b9222b204a2519
7erega <melnyk376@gmail.com>
2018-08-09 17:15:28 +0300

    Add files via upload

---
+++ task-carousel/js/init/owl.js
@@ -0,0 +1,19 @@
+var slider;
+
+slider = function() {
+  if ($(window).width() < 480) {
+    $('.news-list').addClass('owl-carousel');
+    return $('.owl-carousel').owlCarousel({
+      dots: false,
+      margin: 15,
+      items: 1
+    });
+  } else {
+    $('.owl-carousel').trigger('destroy.owl.carousel');
+    return $('.news-list').removeClass('owl-carousel');
+  }
+};
+
+slider();
+
+$(window).resize(slider);
